7b79858a0a40cf54e44cf61e4bae5fad36943d0c
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-26 19:16:18 +0530

    completed the explore section of the app

--- lib/components/today_recipe_list_view.dart
+++ lib/components/today_recipe_list_view.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
-
+// 1
 import '../components/components.dart';
 import '../models/models.dart';
 
 class TodayRecipeListView extends StatelessWidget {
+// 2
   final List<ExploreRecipe> recipes;
-
   const TodayRecipeListView({
     Key? key,
     required this.recipes,
   }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
+// 3
     return Padding(
       padding: const EdgeInsets.only(
         left: 16,
         right: 16,
         top: 16,
       ),
+// 4
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            'Recipes of the Day 🍳',
-            style: Theme.of(context).textTheme.headline1,
-          ),
+// 5
+          Text('Recipes of the Day " ',
+              style: Theme.of(context).textTheme.headline1),
+// 6
           const SizedBox(height: 16),
+// 7
           Container(
             height: 400,
+// 1
             color: Colors.transparent,
+// 2
             child: ListView.separated(
+// 3
               scrollDirection: Axis.horizontal,
+// 4
               itemCount: recipes.length,
+// 5
               itemBuilder: (context, index) {
+// 6
                 final recipe = recipes[index];
                 return buildCard(recipe);
               },
+// 7
               separatorBuilder: (context, index) {
+// 8
                 return const SizedBox(width: 16);
               },
             ),
--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fooderlich_v2/screens/explore_screen.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,8 +12,7 @@ class _HomeState extends State<Home> {
   int _selectedIndex = 0;
 
   static List<Widget> pages = <Widget>[
-    // TODO: Replace with ExploreScreen
-    Container(color: Colors.white),
+    ExploreScreen(),
     // TODO: Replace with RecipesScreen
     Container(color: Colors.green),
     Container(color: Colors.blue),
@@ -38,16 +38,16 @@ class _HomeState extends State<Home> {
         selectedItemColor: Theme.of(context).textSelectionTheme.selectionColor,
         currentIndex: _selectedIndex,
         onTap: _onItemTapped,
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
             icon: Icon(Icons.explore),
             label: 'Explore',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.book),
             label: 'Recipes',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.list),
             label: 'To Buy',
           ),
