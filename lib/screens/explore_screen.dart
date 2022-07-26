55fb8634c2e97bc4155a4caa6a89b802c2f10922
Jitesh117 <sahookumarjitesh@gmail.com>
2022-07-26 11:27:47 +0530

    initial commit

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+import '../components/friend_post_list_view.dart';
+import '../components/today_recipe_list_view.dart';
+import '../models/models.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return ListView(
+            scrollDirection: Axis.vertical,
+            children: [
+              TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
+              const SizedBox(height: 16),
+              FriendPostListView(
+                friendPosts: snapshot.data?.friendPosts ?? [],
+              )
+            ],
+          );
+        } else {
+          return const Center(child: CircularProgressIndicator());
+        }
+      },
+    );
+  }
+}
